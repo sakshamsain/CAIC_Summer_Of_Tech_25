f3a9367e9a9c268210154b46292782b0d4c852df
Saksham saini <sakshamsainisaini94@gmail.com>
2025-05-28 16:42:51 +0530

    setup

---
+++ AI_&_Dev_DevClub/Week-1/saksham-week1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="twitter-classification",
+    version="0.0.1",
+    author="Saksham",
+    author_email="sakshamsainisaini94@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
